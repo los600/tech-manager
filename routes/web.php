3c387900ba0d99a33dab8a6c12137e3dc389b59f
Rubén <rubencaporte@gmail.com>
2021-12-15 09:55:04 +0100

    factory event

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('/', function () { return view('index'); });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
