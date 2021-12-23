93721b281f1e5ab2721f7730eeaa885d3ca2b7d0
Rubén <rubencaporte@gmail.com>
2021-12-23 10:23:42 +0100

    components blade

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+
+    <footer class="bg-dark text-center text-white">
+       
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+          © 2021 Copyright: Tech Event
+        </div>
+        
+      </footer>
