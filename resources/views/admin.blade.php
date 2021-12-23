0dc855ebda8c40e0d2490c5484b54d55e2a1a60e
solidmaradini <solidmaradini@gmail.com>
2021-12-23 11:35:43 +0100

    css Form

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,40 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>ADMINISTRATOR</h1>
-<form action='{{route ('events.create') }}' method="POST">
+<h1 class="card-header">ADMINISTRATOR</h1>
+<form class="card-body" action='{{route ('events.create') }}' method="POST">
   @csrf
-
-    <div class="mb-3">
-      <label for="text" class="form-label">Title</label>
-        <input name= "title" type="text" class="form-control" id="">
-
-        <div class="mb-3">
+  <div class="card-d-flex justify-content-center">
+        <div class="card-body">
+          <label for="text" class="form-label">Title</label>
+          <input name= "title" type="text" class="form-control" id="">
+        </div>
+        <div class="card-body">
             <label  for="text" class="form-label">Date</label>
             <input name= "date" type="text" class="form-control" id="">
         </div>
     
-        <div class="mb-3">
+        <div class="card-body">
             <label for="text" class="form-label">Max Participants</label>
             <input name= "maxparticipants" type="text" class="form-control" id="">
         </div>
 
-      <div class="mb-3">
+      <div class="card-body">
         <label for="text" class="form-label">Description</label>
         <input name= "description" type="text" class="form-control" id="">
       </div>  
 
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="">
+      <div class="card-body-form-check">
+        <input type="checkbox" class="card-form-check-input" id="">
         <label name="isImportant" class="form-check-label" for="exampleCheck1">Important</label>
       </div>
+      <br>
 
-      <div class="form-group">
+      <div class="card-body-form-group">
         <label for="exampleFormControlFile1">Image</label>
         <input name= "image" type="file" class="form-control-file" id="">
       </div>
-    
-        <button type="submit" class="btn btn-primary">Create</button>
-    </div>
+        <br>
+       
+  </div>  
+  <button type="submit" class="card-body-btn btn-primary">Create</button>
 </form>
 @endsection
