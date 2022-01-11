5778aa5bf36a2a5a2b1d02aa65d7e5a95af72f3c
marteta91 <martaguitart938@gmail.com>
2022-01-11 10:24:47 +0100

    testindex

--- tests/Feature/controllers/IndexControllersTest.php
+++ tests/Feature/controllers/IndexControllersTest.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class IndexControllersTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    public function test_importantevents()
+    public function test_user_see_index()
     {
         $response = $this->get('/');
 
