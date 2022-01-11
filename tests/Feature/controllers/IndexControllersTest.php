845be66b34250108aadfdd60649b62da1cdbf23c
martincholt <martindomingo25@gmail.com>
2022-01-11 10:43:40 +0100

    2nd test passed

--- tests/Feature/controllers/IndexControllersTest.php
+++ tests/Feature/controllers/IndexControllersTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Feature\controllers;
 
+use App\Models\Events;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class IndexControllersTest extends TestCase
-{
+{   use RefreshDataBase;
     /**
      * A basic feature test example.
      *
@@ -19,4 +21,15 @@ class IndexControllersTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_user_can_see_all_events(){
+        User::factory()->create();
+        $events = Events::factory(3)->create();
+
+        $response = $this->get('/');
+
+        $response->assertSee ($events [0]-> title);
+    }
+
+
 }
