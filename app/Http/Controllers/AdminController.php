06d708e14f7419ed9f9fc751227642f317c18f67
Rubén <rubencaporte@gmail.com>
2022-01-13 14:21:47 +0100

    create admin page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,9 +6,9 @@ use App\Models\Events;
 
 class AdminController extends Controller{
 
-    public function index() {
-
-        return 'Admin';
+    public function admin(){
+        $events = Events::all(); // trayedo de la base de datos todos los eventos
+        return view('admin');// pasando los datos a la vista, y pediendo los datos que nos muestre.
     }
 
 }
