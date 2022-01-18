e3ae64d4d529de5486da841f15d534ab20bf7863
Rubén <rubencaporte@gmail.com>
2022-01-18 12:26:20 +0100

    corrousel forelse

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,64 +9,47 @@
 
   <div id="myCarousel" class="carousel slide pointer-event" data-bs-ride="carousel">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="" aria-current="true"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
     </div>
 
-    @foreach ($events as $event )
+
+
+    <div class="carousel-inner">
       
-    
-    <div class="carousel-inner" role="listbox">  
-      <div class="carousel-item active">
-        <img src="{{ asset('storage').'/'.$events[0]->image  }}" class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect><>
+      @forelse ($events as $event )
+           <div class="carousel-item @if ($loop->index==0) active @endif">
+            <img src="{{ asset('storage').'/'.$event->image }}" class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect><>
+
         <div class="container">
           <div class="carousel-caption">
-            <h1>{{$events[0]->title}}</h1>
-            <p>{{$events[0]->description}}</p>
-    
-            {{--     @auth
-            @php
-              if (Auth::user()->isSubscribed($event)){$btnSus= 'Inscrito!';}
-              if (!Auth::user()->isSubscribed($event)){$btnSus= 'Suscribirse';}
-            @endphp 
-            <p><button class="btn btn-lg btn-primary" href="/">{{$btnSus}}</button></p>
-        @endauth      --}}      
-           
-            
+            <h1>{{ $event->title }}.</h1>
+            <p>{{ $event->description }}</p>
           </div>
         </div>
       </div> 
-      @endforeach
+    
+        
+      @empty
+     @endforelse
+
           
         
-      @foreach ($events as $event)
-        
+      @forelse ($events as $event )
       <div class="carousel-item">
         <img src="{{ asset('storage').'/'.$event->image }}" class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect><>
-        
+
         <div class="container">
           <div class="carousel-caption text-start">
             <h1>{{ $event->title }}.</h1>
             <p>{{ $event->description }}</p>
-      
-            {{--     @auth
-          @php
-          if (Auth::user()->isSubscribed($event)){$btnSus= 'Inscrito!';}
-          if (!Auth::user()->isSubscribed($event)){$btnSus= 'Suscribirse';}
-        @endphp
-                   
-           <p><button class="btn btn-lg btn-primary" href="/">{{$btnSus}}</button></p> 
-          
-          @endauth   --}}
-                   
-            
           </div>
         </div>
       </div>
-     
-      @endforeach
-      
+      @empty
+      @endforelse
+    
     </div>
 
     <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
@@ -96,15 +79,9 @@
               <p class="lead">{{ $event->description }}</p>
               <p class="lead">{{$event ->image}}</p>
             </div>
-            @auth
-            @php
-              if (Auth::user()->isSubscribed($event)){$btnSus = "Inscrito!";}
-              if (!Auth::user()->isSubscribed($event)){$btnSus = "Suscribirse";}//minuto 42.58 (laravel many to many)
-              
-            @endphp
-                    
-            <p><button class="btn btn-lg btn-primary" href="/">{{$btnSus}}</button></p>
-           @endauth  
+              <div class="container-btn">
+                <a class="btn btn-lg btn-primary" href="https://getbootstrap.com/docs/5.1/examples/carousel/#">Subscribe</a>
+              </div>
     
       </div>
     </div>
@@ -117,9 +94,3 @@
 
  
 @endsection
-
-   
-
-      
-  
-
