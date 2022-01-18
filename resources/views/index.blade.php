a4fd749e0eba68709645fbfe10a7fe97640839be
martincholt <martindomingo25@gmail.com>
2022-01-18 14:15:56 +0100

    cards ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -62,9 +62,9 @@
     </button>
   </div>
 
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-2 justify-content-center align-self-center">
       @foreach ($events as $event )
-      <div class="card" style="width: 18rem;">
+      <div class="card m-5"  style="width: 18rem;">
         <img class="card-img-top" src="{{ asset('storage').'/'.$event->image }}" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{ $event->title }}</h5>
